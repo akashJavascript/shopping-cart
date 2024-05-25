34dd5fea479fb6d1670c81366670070a9ef97492
Akash <akashrangaswamy2010@gmail.com>
2024-05-25 11:06:26 -0700

    feat: able to add to cart and redid styling for ProductCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,20 +1,67 @@
 import PropTypes from 'prop-types';
 import '../styles/ProductCard.css';
-const ProductCard = ({ imgSrc, price, name, addToCartFunction, id }) => {
+import { useState } from 'react';
+
+const ProductCard = ({ imgSrc, price, name, addToCart, id }) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const incrementQuantity = () => {
+    setQuantity(prevQuantity => prevQuantity + 1);
+  };
+
+  const decrementQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(prevQuantity => prevQuantity - 1);
+    }
+  };
+
   return (
-    <div className="shop-card" style={{ backgroundImage: `url(${imgSrc})` }}>
-      <p className="item-name">{name}</p>
-      <p className="price">${price}</p>
-      <div className="add-cart-container">
-        <label htmlFor="quantity">Quantity</label>
-        <input type="number" name="quantity" id="quantity" />
-        <button className="add-cart-button">Add to Cart</button>
+    <div className="shop-card">
+      <div className="card-top">
+        <p className="item-name">{name}</p>
+      </div>
+      <div
+        className="card-image"
+        style={{ backgroundImage: `url(${imgSrc})` }}
+      ></div>
+      <div className="card-bottom">
+        <p className="shop-price">${price}</p>
+        <div className="add-cart-container">
+          <div className="quantity-container">
+            <button className="decrement-button" onClick={decrementQuantity}>
+              -
+            </button>
+            <input
+              type="number"
+              name="quantity"
+              id="quantity"
+              value={quantity}
+              onChange={e =>
+                setQuantity(e.target.value === '' ? '' : Number(e.target.value))
+              }
+            />
+            <button className="increment-button" onClick={incrementQuantity}>
+              +
+            </button>
+          </div>
+          <button
+            className="add-cart-button"
+            onClick={() => addToCart({ id: id, quantity: Number(quantity) })}
+          >
+            Add
+          </button>
+        </div>
       </div>
     </div>
   );
 };
+
 export default ProductCard;
+
 ProductCard.propTypes = {
   imgSrc: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  addToCart: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired,
 };
--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,29 +1,33 @@
 import ProductCard from '../components/ProductCard';
 import { UseGetItems } from '../hooks/getItems';
 import { useState } from 'react';
+import NavbarShop from '../components/NavbarShop';
+import '../styles/ShopPage.css';
 const ShopPage = () => {
   let items = UseGetItems(20);
-  let [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([]);
+  const itemCount = cart.reduce((total, item) => total + item.quantity, 0);
   function addToCart(item) {
     setCart([...cart, item]);
   }
   return (
     <div>
-      <h1>Shop Page</h1>
-      {items.map(item => {
-        return (
-          <ProductCard
-            key={item.id}
-            id={item.id}
-            price={item.price}
-            imgSrc={item.image}
-            name={item.title}
-            addToCartFunction={function () {
-              addToCart(item);
-            }}
-          />
-        );
-      })}
+      <NavbarShop itemCount={itemCount}></NavbarShop>
+      <h1 className="shop-header">Shop</h1>
+      <div className="items-container">
+        {items.map(item => {
+          return (
+            <ProductCard
+              key={item.id}
+              id={item.id}
+              price={item.price}
+              imgSrc={item.image}
+              name={item.title}
+              addToCart={addToCart}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
